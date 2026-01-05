c79b3b3dd62d4079356dd27c45f544c3c193a67d
Aran Borkum <aran.borkum@kraken.tech>
2026-01-05 15:22:22 +0000

    Update to cache-register-rs

--- src/django_stream/django_app/registers.py
+++ src/django_stream/django_app/registers.py
@@ -1,6 +1,6 @@
-from cache_register import register
+import cache_register
 
 from django_stream.django_app.base import handler, serializer
 
-serializers = register.Register[serializer.EventSerializer]("serializers")
-handlers = register.Register[handler.Handler]("handlers")
+serializers = cache_register.Register[serializer.EventSerializer]("serializers")
+handlers = cache_register.Register[handler.Handler]("handlers")
