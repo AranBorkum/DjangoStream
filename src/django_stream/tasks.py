c983f9f8488c65a93d330d7c6e23b7e2dbab9618
Aran Borkum <aranborkum@hotmail.com>
2026-01-27 08:56:04 +0000

    Revert "Update inbound handler"

--- src/django_stream/tasks.py
+++ src/django_stream/tasks.py
@@ -1,12 +1,10 @@
-import datetime
-import json
 import logging
 import typing
 
 from celery import shared_task
 from celery.exceptions import SoftTimeLimitExceeded
 
-from django_stream import constants, entities, usecases
+from django_stream import usecases
 
 logger = logging.getLogger(__name__)
 
@@ -25,20 +23,8 @@ def process_event(self: typing.Any, event: dict[str, typing.Any]) -> None:
     Idempotent SQS-backed Celery task.
     """
     try:
-        payload = json.loads(event["body"])
-        event_entity = entities.Event(
-            id=event["id"],
-            created_at=datetime.datetime.now(),
-            updated_at=datetime.datetime.now(),
-            payload=payload["payload"],
-            event_type=payload["event_type"],
-            queue=payload["queue"],
-            trace_id=event["trace_id"],
-            status=constants.InboundEventStatus.PENDING,
-            timestamp=datetime.datetime.fromisoformat(event["timestamp"]),
-        )
         usecase = usecases.ProcessInboundEvent()
-        usecase.process(event=event_entity)
+        usecase.process(event=event)
 
     except SoftTimeLimitExceeded:
         logger.warning("Soft time limit exceeded for %s", event)
